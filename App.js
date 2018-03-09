06223cff6e16134421947c2dcb8e32562ba5faf1
Dominic Lee <leemflee@gmaill.com>
2018-03-09 18:35:26 +0800

    https://appendto.com/2017/11/build-simple-chat-app-react-native-firebase/

---
+++ App.js
@@ -0,0 +1,18 @@
+import { Navigation } from "react-native-navigation";
+
+import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+
+// Register Screens
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+
+// Start a App
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
+  }
+});
---
+++ android/app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD_6c_0F3zhirpopNcemApJELDgF4zUfRU",
+    authDomain: "njac-5f0e8.firebaseapp.com",
+    databaseURL: "https://njac-5f0e8.firebaseio.com",
+    projectId: "njac-5f0e8",
+    storageBucket: "njac-5f0e8.appspot.com",
+    messagingSenderId: "4358170829"
+};
